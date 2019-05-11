d0b92cf2acd9d774adac6cbf543b9797a14faabb
AsukaFrontier <linkin9411@icloud.com>
2019-05-11 15:26:03 +0800

    Commit Insertion.java

--- src/Sorts/Insertion.java
+++ src/Sorts/Insertion.java
@@ -11,7 +11,15 @@ package Sorts;
 public class Insertion {
     public static void sort(Comparable[] a)
     {
-        //
+        for(int i=1;i<a.length;i++)
+        {
+            int j=i;
+            while(j>0&&less(a[j],a[j-1]))
+            {
+                exch(a,j,j-1);
+                j--;
+            }
+        }
     }
     public static boolean isSorted(Comparable[] a)
     {
