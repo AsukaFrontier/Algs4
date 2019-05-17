88ae42c61af76ada79b95a1e6a539340e236932b
AsukaFrontier <linkin9411@icloud.com>
2019-05-17 12:57:20 +0800

    IndexMinPQ

---
+++ src/Sorts/IndexMinPQ.java
@@ -0,0 +1,17 @@
+package Sorts;
+/**
+ *IndexMinPQ(int maxN)
+ * void insert(int k, Item item)
+ * void change(int k, Item item)
+ * boolean contains(int k)
+ * void delete(int k)
+ * Item min()
+ * int minIndex()
+ * int delMin()
+ * boolean isEmpty()
+ * int size()
+ */
+public class IndexMinPQ<Item extends Comparable<Item>>
+{
+    //
+}
