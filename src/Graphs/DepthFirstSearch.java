c3a071bdddbf3e33e806b3754042331b9bf1e9e4
AsukaFrontier <linkin9411@icloud.com>
2019-05-21 16:16:57 +0800

    add annotations; add TestSearch.java;

--- src/Graphs/DepthFirstSearch.java
+++ src/Graphs/DepthFirstSearch.java
@@ -1,10 +1,17 @@
 package Graphs;
-
+/**
+ * private boolean[] marked;
+ * private int count;
+ * public DepthFirstSearch(Graph G, int s)
+ * private void dfs(Graph G, int v)
+ * public boolean marked(int w)
+ * public int count()
+ */
 public class DepthFirstSearch {
     //
     private boolean[] marked;
     private int count;
-    public DepthFirstSearch(Graph G, int s)
+    public DepthFirstSearch(Graph G, int s)//找到和起点s连通的所有顶点
     {
         marked=new boolean[G.V()];
         dfs(G,s);
@@ -13,15 +20,15 @@ public class DepthFirstSearch {
     {
         marked[v]=true;
         count++;
-        for(int w: G.adj(v))
+        for(int w: G.adjacent(v))
             if(!marked[w])
                 dfs(G,w);
     }
-    public boolean marked(int w)
+    public boolean marked(int w)//v和s是连通的吗
     {
         return marked[w];
     }
-    public int count()
+    public int count()//与s连通的顶点总数
     {
         return count;
     }
--- src/Graphs/Search.java
+++ src/Graphs/Search.java
@@ -1,5 +1,4 @@
 package Graphs;
-
 /**
  * Searching(Graphs G, int s)
  * boolean marked(int v)
