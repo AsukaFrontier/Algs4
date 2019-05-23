eaea3583a2825eb22a8be337cc63952a9509344a
AsukaFrontier <linkin9411@icloud.com>
2019-05-23 12:34:23 +0800

    Add Transaction.java;

---
+++ src/Fundamentals/Transaction.java
@@ -0,0 +1,16 @@
+package Fundamentals;
+
+/**
+ *Transaction(String who, Date when, double amount)
+ *Transaction(String transaction)
+ *String who()
+ * Date when()
+ * double amount()
+ * String toString()
+ * boolean equals(Object that)
+ * int compareTo(Transaction that)
+ * int hashCode()
+ */
+public class Transaction{
+    //
+}
