c50a700f40247e80e2ec6cfa34e720f9083aeb81
AsukaFrontier <linkin9411@icloud.com>
2019-05-23 12:58:24 +0800

    Delete Paths, Search.java;

--- src/Graphs/Paths.java
+++
@@ -1,9 +0,0 @@
-package Graphs;
-
-/**
- * Paths(Graphs G, int s)
- * boolean hasPathTo(int v)
- * Iterable<Integer> pathTo(int v)
- */
-public class Paths {
-}
--- src/Graphs/Search.java
+++
@@ -1,8 +0,0 @@
-package Graphs;
-/**
- * Searching(Graphs G, int s)
- * boolean marked(int v)
- * int count()
- */
-public class Search {
-}
