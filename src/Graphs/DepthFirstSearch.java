8bc07888f40be22532b87eaa745996a62309f535
AsukaFrontier <linkin9411@icloud.com>
2019-05-29 19:35:14 +0800

    revise DepthFirstSearch;

--- src/Graphs/DepthFirstSearch.java
+++ src/Graphs/DepthFirstSearch.java
@@ -9,13 +9,13 @@ package Graphs;
  * public int count()                     //与s连通的顶点总数
  */
 public class DepthFirstSearch {
-    //
-    private boolean[] marked;//声明
-    private int count;//声明
+    //给定一个连通图，起点s到其他所有顶点都是连通的，marked[]中的所有元素值都将为true;
+    private boolean[] marked;//记录和起点连通的所有顶点；
+    private int count;//
     public DepthFirstSearch(Graph G, int s)//找到和起点s连通的所有顶点
     {
         marked=new boolean[G.V()];
-        dfs(G,s);
+        dfs(G,s);//从起点开始，深度优先搜索；
     }
     private void dfs(Graph G, int v)
     {
@@ -25,7 +25,7 @@ public class DepthFirstSearch {
             if(!marked[w])
                 dfs(G,w);
     }
-    public boolean marked(int w)//v和s是连通的吗
+    public boolean marked(int w)//w和s是连通的吗
     {
         return marked[w];
     }
