51ad430958b90677e52a0f4e8beb0252dade8bf2
AsukaFrontier <linkin9411@icloud.com>
2019-06-04 21:01:15 +0800

    revise DirectedCycle;

--- src/DirectedGraphs/DirectedCycle.java
+++ src/DirectedGraphs/DirectedCycle.java
@@ -25,7 +25,26 @@ public class DirectedCycle//寻找有向环
     }
     private void dfs(Digraph G, int v)
     {
-        //
+        onStack[v]= true;//v进入递归调用的栈中
+        marked[v]=true;
+        for(int w: G.adj(v))
+            if(this.hasCycle())//G含有有向环
+                return ;
+            else if(!marked[w])
+            {
+                edgeTo[w]=v;//v->w
+                dfs(G,w);
+            }
+            else if(onStack[w])//当前访问的w，在此之前已经被dfs递归调用过；说明存在环；
+            //将路径压入栈中
+            {
+                cycle= new Stack<Integer>();
+                for(int x=v;x!=w;x=edgeTo[x])
+                    cycle.push(x);
+                cycle.push(w);
+                //cycle.push(v);
+            }
+        onStack[v]= false;//dfs()的最后，dfs(v)结束调用；
     }
     public boolean hasCycle()//G是否含有有向环
     {
