865d7c2322764a31e6327677d0fb4e52f799c23c
AsukaFrontier <linkin9411@icloud.com>
2019-06-04 17:49:27 +0800

    revise Digraph;

--- src/DirectedGraphs/Digraph.java
+++ src/DirectedGraphs/Digraph.java
@@ -1,4 +1,55 @@
 package DirectedGraphs;
 
+import Fundamentals.Bag;
+
+/**
+ * private final int V;
+ * private int E;
+ * private Bag<Intger>[] adj;
+ *
+ * public Digraph(int V)
+ * public int V()
+ * public int E()
+ * public void addEdge(nt v, int w)
+ * public Iterable<Integer> adj(int v)
+ * public Digraph reverse()
+ */
 public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V)
+    {
+        this.V=V;
+        this.E=0;
+        adj= (Bag<Integer>[]) new Bag[V];
+        for(int v=0;v<V;v++)
+            adj[v]= new Bag<Integer>();
+    }
+    public int V()
+    {
+        return V;
+    }
+    public int E()
+    {
+        return E;
+    }
+    public void addEdge(int v, int w)//v->w
+    {
+        adj[v].add(w);
+        E++;
+    }
+    public Iterable<Integer> adj(int v)
+    {
+        return adj[v];
+    }
+    public Digraph reverse()
+    {
+        Digraph R= new Digraph(V);
+        for(int v=0;v<V;v++)
+            for(int w: adj[v])
+                R.addEdge(w,v);//把 v->w reverse 为 w->v
+        return R;
+    }
 }
