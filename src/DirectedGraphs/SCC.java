967d1ee7009ffd6962018aa452b8dcc555da5b0f
AsukaFrontier <linkin9411@icloud.com>
2019-06-05 14:51:12 +0800

    revise SCC;

--- src/DirectedGraphs/SCC.java
+++ src/DirectedGraphs/SCC.java
@@ -1,7 +1,45 @@
 package DirectedGraphs;
+/**
+ *
+ */
 public class SCC
 //强连通分量
 //Strongly Connected Components
+//KosarajuCC
 {
-    //
+    private boolean[] marked; //已访问过的顶点
+    private int[] id; //强连通分量的标识符
+    private int count; //强连通分量的数量
+    public SCC(Digraph G)
+    {
+        marked= new boolean[G.V()];
+        id= new int[G.V()];
+        DepthFirstOrder order= new DepthFirstOrder(G.reverse()); //有向图的反向图
+        for(int s: order.reversePost()) //反向图的逆后序序列
+            if(!marked[s])
+            {
+                dfs(G,s);
+                count++;
+            }
+    }
+    private void dfs(Digraph G, int v)
+    {
+        marked[v]= true;
+        id[v]= count;
+        for(int w: G.adj(v))
+            if(!marked[w])
+                dfs(G,w);
+    }
+    public boolean stronglyConnected(int v, int w)
+    {
+        return id[v]== id[w];
+    }
+    public int id(int v)
+    {
+        return id[v];
+    }
+    public int count()
+    {
+        return count;
+    }
 }
