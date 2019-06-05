582f87449cb0dfc87296c4221fbe4fe2dfc64007
AsukaFrontier <linkin9411@icloud.com>
2019-06-05 12:36:24 +0800

    Add DepthFirstOrder; add Topological;

---
+++ src/DirectedGraphs/DepthFirstOrder.java
@@ -0,0 +1,8 @@
+package DirectedGraphs;
+/**
+ *
+ */
+public class DepthFirstOrder //有向图中基于深度优先搜索的顶点排序
+{
+
+}
---
+++ src/DirectedGraphs/Topological.java
@@ -0,0 +1,9 @@
+package DirectedGraphs;
+/**
+ *
+ */
+public class Topological //优先级限制下的调度问题，等价于，计算有向无环图中的所有顶点的拓扑顺序
+//当且仅当一幅有向图是无环图时，它才能进行拓扑排序
+{
+    //
+}
