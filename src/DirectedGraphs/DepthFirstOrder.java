6f4b5b50031c9cd3a3e0559591efff4974c9c55d
AsukaFrontier <linkin9411@icloud.com>
2019-06-05 12:55:36 +0800

    revise DepthFirstOrder; revise Topological;

--- src/DirectedGraphs/DepthFirstOrder.java
+++ src/DirectedGraphs/DepthFirstOrder.java
@@ -1,8 +1,36 @@
 package DirectedGraphs;
+import Fundamentals.Queue;
+import Fundamentals.Stack;
 /**
+ * private boolean[] marked;
+ * private Queue<Integer> pre;
+ * private Queue<Integer> post;
+ * private Stack<Integer> reversePost;
  *
+ * public DepthFirstOrder(Digraph G)
+ * private void dfs(Digraph G, int v)
+ * public Iterable<Integer> pre()
+ * public Iterable<Integer> post()
+ * public Iterable<Integer> reversePost()
  */
-public class DepthFirstOrder //有向图中基于深度优先搜索的顶点排序
+public class DepthFirstOrder//有向图中基于深度优先搜索的顶点排序
 {
-
+    private boolean[] marked;
+    private Queue<Integer> pre;
+    private Queue<Integer> post;
+    private Stack<Integer> reversePost;
+    public DepthFirstOrder(Digraph G)
+    {
+        pre= new Queue<Integer>();
+        post= new Queue<Integer>();
+        reversePost= new Stack<Integer>();
+        marked= new boolean[G.V()];
+        for(int v=0;v<G.V();v++)
+            if(!marked[v])
+                dfs(G,v);
+    }
+    private void dfs(Digraph G, int v)
+    {
+        //
+    }
 }
