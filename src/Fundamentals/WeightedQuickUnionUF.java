642331aaee9f1d5db79a2d441085cca44338a1d2
AsukaFrontier <linkin9411@icloud.com>
2019-07-16 16:33:05 +0800

    revise WeightedQuickUnionUF;

--- src/Fundamentals/WeightedQuickUnionUF.java
+++ src/Fundamentals/WeightedQuickUnionUF.java
@@ -3,12 +3,18 @@ package Fundamentals;
 public class WeightedQuickUnionUF implements UnionFind //
 {
     //class variables
-    private int[] id;
-    private int[] sz;
+    private int[] id; //父链接数组
+    private int[] sz; //各个根结点所对应的分量的大小
     private int count;
     public WeightedQuickUnionUF(int N)
     {
-        //
+        count= N;
+        id= new int[N];
+        for(int i=0;i<N;i++)
+            id[i]= i;
+        sz= new int[N];
+        for(int i=0;i<N;i++) //初始分量大小: 1
+            sz[i]=1;
     }
     public int count()
     {
@@ -20,10 +26,26 @@ public class WeightedQuickUnionUF implements UnionFind //
     }
     public int find(int p)
     {
-        //
+        while(p!=id[p])
+            p= id[p];
+        return p;
     }
     public void union(int p, int q)
     {
-        //
+        int i= find(p);
+        int j= find(q);
+        if(i==q)
+            return ;
+        if(sz[i]<sz[j])//i所在的树的大小 小于 j所在的树的大小
+        {
+            id[i]=j;
+            sz[j]=sz[i]+sz[j];
+        }
+        else
+        {
+            id[j]=i;
+            sz[i]=sz[j]+sz[i];
+        }
+        count--;
     }
 }
