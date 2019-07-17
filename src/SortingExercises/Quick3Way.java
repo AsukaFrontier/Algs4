ad12ba4b2e6b39f56997cd3f36a16dea434bee3c
AsukaFrontier <linkin9411@icloud.com>
2019-07-17 17:23:59 +0800

    add Quick3Way;

---
+++ src/SortingExercises/Quick3Way.java
@@ -0,0 +1,31 @@
+package SortingExercises;
+public class Quick3Way implements Sortable //
+{
+    @Override
+    public void sort(Comparable[] a)
+    {
+        RandomShuffle.shuffle(a);
+        sort(a,0,a.length-1);
+    }
+    private void sort(Comparable[] a, int lo, int hi)
+    {
+        if(hi<=lo)
+            return ;
+        int lt= lo;
+        int i= lo+1;
+        int gt= hi;
+        Comparable v= a[lo];
+        while(i<=gt)
+        {
+            int cmp=a[i].compareTo(v);
+            if(cmp<0)
+                exch(a,lt++,i++);
+            else if(cmp>0)
+                exch(a,i,gt--);
+            else
+                i++;
+        }
+        sort(a,lo,lt-1);
+        sort(a,gt+1,hi);
+    }
+}
