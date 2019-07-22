c1e2e4c042a7007c1d655724fb881334533eafff
AsukaFrontier <linkin9411@outlook.com>
2019-07-22 23:30:42 +0800

    revise BST;

--- src/Searching/BST.java
+++ src/Searching/BST.java
@@ -1,15 +1,15 @@
 package Searching;
-
 public class BST<Key extends Comparable<Key>, Value> implements OrderedST<Key, Value> //
 {
+    //实现BinarySearchTree查找算法所需的数据结构: binary search tree(二叉查找树)
     private class Node
     {
         private Key key;
         private Value val;
         private Node left;
         private Node right;
-        private int N; //以该节点为根的子树中的节点总数
-        public Node(Key key, Value val, int N)
+        private int N; //以该节点为根的子树中的节点总数(包括该节点)
+        public Node(Key key, Value val, int N) //a constructor of Node;
         {
             this.key= key;
             this.val= val;
@@ -17,13 +17,14 @@ public class BST<Key extends Comparable<Key>, Value> implements OrderedST<Key, V
             //by default, left=null, right=null;
         }
     }
-    private Node root;
-    //
+    //类的实例域
+    private Node root; //该类所创建的每一个实例对象都是一个树
+    //类的实例方法
     public int size()
     {
-        return size(root);
+        return size(root); //根节点的节点总数
     }
-    private int size(Node x)
+    private int size(Node x) //节点x所在的子树的节点总数
     {
         if(x==null)
             return 0;
@@ -40,7 +41,10 @@ public class BST<Key extends Comparable<Key>, Value> implements OrderedST<Key, V
     {
         //终止递归的条件: 到达树的叶子节点后仍未找到key对应的节点, 则创建新节点;
         if(x==null) //该子树为空, 则在该处新建一个节点
-            return new Node(key, val, 1);// private int N: 以该节点为根的子树中的节点总数
+        {
+            Node newSubTree= new Node(key, val, 1);// private int N: 以该节点为根的子树中的节点总数
+            return newSubTree;
+        }
         //递归查找
         int cmp= key.compareTo(x.key);
         if(cmp==0) //命中
@@ -59,6 +63,7 @@ public class BST<Key extends Comparable<Key>, Value> implements OrderedST<Key, V
     }
     private Value get(Node x, Key key) //recursive implementation;
     {
+        //递归结束的条件之一: 当前已访问到叶子节点, 未命中;
         if(x==null)
             return null;
         int cmp= key.compareTo(x.key);
@@ -72,15 +77,62 @@ public class BST<Key extends Comparable<Key>, Value> implements OrderedST<Key, V
     }
     public Key min()
     {
-        //
+        if(min(root)==null)
+            return null;
+        else
+            return min(root).key;
+    }
+    private Node min(Node x)
+    {
+        if(x==null)
+            return null;
+        if(x.left==null)
+            return x;
+        else
+            return min(x.left);
     }
     public Key max()
     {
-        //
+        if(max(root)==null)
+            return null;
+        else
+            return max(root).key;
+    }
+    private Node max(Node x)
+    {
+        if(x==null)
+            return null;
+        if(x.right==null)
+            return x;
+        else
+            return max(x.right);
     }
     public Key floor(Key key)
     {
-        //
+        Node x= floor(root, key);
+        if(x==null)
+            return null;
+        else
+            return x.key;
+    }
+    private Node floor(Node x, Key key)
+    {
+        if(x==null)
+            return null;
+        int cmp= key.compareTo(x.key);
+        if(cmp<0)
+            return floor(x.left, key);
+        else
+            if(cmp==0)
+                return x;
+            else
+            {
+                Node temp= min(x.right);
+                if(temp.key.compareTo(key)>0)
+                    return x;
+                else
+                    return floor(x.right, key);
+            }
     }
     public Key ceiling(Key key)
     {
